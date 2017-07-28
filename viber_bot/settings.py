18781345efb7e29c183832a3f47a003e2ec1a05f
Fire <sablin_roman@yahoo.com>
2017-07-28 23:14:55 +0800

    123

--- viber_bot/settings.py
+++ viber_bot/settings.py
@@ -14,7 +14,7 @@ import os
 from decouple import config
 import dj_database_url
 
-#DATABASES = { 'default': dj_database_url.config(default=config('DATABASE_URL')) }
+DATABASES = { 'default': dj_database_url.config(default=config('DATABASE_URL')) }
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'viber_bot.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -87,7 +87,7 @@ DATABASES = {
         'HOST': config('DB_HOST'),
         'PORT': '5432'
     }
-}
+}"""
 
 
 # Password validation
